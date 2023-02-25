0bc8637ab0339448db7bac0703eebb0a9b6387c0
Unimatrix099 <adriancuzman@gmail.com>
2023-02-25 17:08:32 +0200

    updated homing feedrate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2112,7 +2112,7 @@
 #endif
 
 // Homing speeds (linear=mm/min, rotational=°/min)
-#define HOMING_FEEDRATE_MM_M { (50*60), (50*60), (4*60) }
+#define HOMING_FEEDRATE_MM_M { (50*60), (50*60), (50*60) }
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
