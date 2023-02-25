0fdcab06b2a5498ebf1ffc18f5cd1a2e6ca6c6d5
Unimatrix099 <adriancuzman@gmail.com>
2023-02-25 15:13:34 +0200

    switch P2_26 as FAN pin, from HEATER pin

--- Marlin/src/pins/lpc1768/pins_MKS_SBASE.h
+++ Marlin/src/pins/lpc1768/pins_MKS_SBASE.h
@@ -98,7 +98,7 @@
 //
 #define HEATER_BED_PIN                     P2_05
 #define HEATER_0_PIN                       P2_07
-#define HEATER_1_PIN                       P2_06
+// #define HEATER_1_PIN                       P2_06
 #ifndef FAN_PIN
   #define FAN_PIN                          P2_04
 #endif
