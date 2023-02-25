50131060661f3896ec80edebee20410ee0f6f108
Unimatrix099 <adriancuzman@gmail.com>
2023-02-25 20:03:56 +0200

    increased AMPs for motors to 1.1A

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1320,7 +1320,7 @@
 
   // Actual motor currents in Amps. The number of entries must match DIGIPOT_I2C_NUM_CHANNELS.
   // These correspond to the physical drivers, so be mindful if the order is changed.
-  #define DIGIPOT_I2C_MOTOR_CURRENTS { 1.0, 1.0, 1.0, 1.5, 1.0 } // MKS_SBASE
+  #define DIGIPOT_I2C_MOTOR_CURRENTS { 1.1, 1.1, 1.1, 1.5, 1.0 } // MKS_SBASE
 
   //#define DIGIPOT_USE_RAW_VALUES    // Use DIGIPOT_MOTOR_CURRENT raw wiper values (instead of A4988 motor currents)
 
